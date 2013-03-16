7d7ad7cb6a18f0def5e35d252e2b81e011abb446
Colby Rabideau <crabideau5691@gmail.com>
2013-03-16 01:15:39 -0400

    save files!

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var config = require('./config');
 var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var mkdirp = require('mkdirp');
 
 var app = express();
 
 app.configure(function(){
   app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/webory-bank'));
+  app.use(express.static(__dirname + '/webowy-bank'));
   app.use(app.router);
 });
 
@@ -13,8 +16,25 @@ app.get('/', function (req, res) {
   res.send('Welcome to the ' + config.name);
 });
 
-app.post('/weborize', function (req, res) {
-  res.send('webowizing!');
+app.post('/webowize', function (req, res) {
+  var name = req.param('name').split('/');
+  var file;
+
+  mkdirp('webowy-bank/' + name.slice(0, -1).join('/'), function (err) {
+    file = fs.createWriteStream('webowy-bank/' + name.join('/'));
+    if (err) {
+      res.send('shit! ' + err);
+    } else {
+      http.get(req.param('url'), function(response, err) {
+        if (err) {
+          res.send('shit2! ' + err);
+        } else {
+          response.pipe(file);
+          res.send('success');
+        }
+      });
+    }
+  });
 });
 
 app.listen(config.server.port);
